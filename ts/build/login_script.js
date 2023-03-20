978c4632f806a2dda1891376d6f951ea3a0511d3
Shiv Chveli <shivchevli9212@gmail.com>
2023-03-20 10:52:32 +0530

    Improve focus Styleing and management

--- ts/build/login_script.js
+++ ts/build/login_script.js
@@ -5,4 +5,13 @@ btn_menu.addEventListener("click", (event) => {
     input.setAttribute("type", isShow ? "input" : "password");
     isShow = !isShow;
 });
+let t1 = document.querySelector(".select-btn-group").querySelectorAll("[type='radio']");
+t1.forEach((el) => {
+    el.addEventListener("focusin", (event) => {
+        el.parentElement.classList.add("focus");
+    });
+    el.addEventListener("focusout", (event) => {
+        el.parentElement.classList.remove("focus");
+    });
+});
 //# sourceMappingURL=login_script.js.map
--- ts/login_script.ts
+++ ts/login_script.ts
@@ -6,3 +6,13 @@ btn_menu.addEventListener("click", (event) => {
     input.setAttribute("type", isShow ? "input" : "password");
     isShow = !isShow;
 });
+
+let t1 = document.querySelector(".select-btn-group").querySelectorAll("[type='radio']");
+t1.forEach((el:HTMLInputElement) => {
+    el.addEventListener("focusin", (event) => {
+        el.parentElement.classList.add("focus");
+    })
+    el.addEventListener("focusout",(event) => {
+        el.parentElement.classList.remove("focus");
+    })
+})
